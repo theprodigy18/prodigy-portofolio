f1e5806026c64a443e0c5e6eba88640ccd44b784
theprodigy18 <168449386+theprodigy18@users.noreply.github.com>
2024-12-23 14:37:46 +0700

    Change profile and footer

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // Menonaktifkan linting saat build
+  },
 };
 
 export default nextConfig;
